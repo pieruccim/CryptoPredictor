62808d4d1d45c9a7b62c299918313429e07828f6
enricollen <nello.enrico@gmail.com>
2022-03-16 13:18:01 +0100

    updated the currencies in .env + minor changes

--- fantoccio.py
+++ fantoccio.py
@@ -1,14 +1,59 @@
+import matplotlib
+import pandas
+from plotly.tools import mpl_to_plotly
+from matplotlib import pyplot as plt
+import dash
+from dash import html, dcc
+import plotly.graph_objects as go
+
+
 from persistence.MongoConnector import MongoConnector
-from updater import Updater
+from utilities.utils import Utils
 
-#result = MongoConnector.get_collection('bitcoin').find()
-#print(result.next())
+# coll_name = Utils.load_config("COLLECTION_NAME")
+# result = MongoConnector.get_collection(coll_name).find()
+# print(result.next())
 
-up = Updater()
-#dataframe = up.get_missing_tuples('BTC')
+app = dash.Dash()
 
-#final_df = up.compute_statistical_values(original_df=dataframe)
+def plot_graph(dataset):
+    plt1 = go.Figure()
+    plt1.title('BTC-USD Adj Close Price', fontsize=16)
+    plt1.xlabel("Date")
+    plt1.ylabel("Adjusted Price")
 
-#up.store_missing_tuples('BTC', final_df)
+    ax = dataset['adj_close'].plot(lw=3, figsize=(14, 7), label='Original observations')
+    dataset['ema_short'].plot(ax=ax, lw=3, label='EMA (window ' + str(2) + ')')
+    dataset['ema_long'].plot(ax=ax, lw=3, label='EMA (window ' + str(5) + ')')
 
-up.update_currencies_collections()
+    plt1.tick_params(labelsize=12)
+    plt1.legend(loc='upper left', fontsize=12)
+
+    plt1.plot(dataset.loc[dataset.trend == 1.0].index,
+             dataset.adj_close[dataset.trend == 1.0],
+             '^', markersize=8, color='g', label='up')
+
+    plt1.plot(dataset.loc[dataset.trend == 0.0].index,
+             dataset.adj_close[dataset.trend == 0.0],
+             '^', markersize=8, color='y', label='flat')
+
+    plt1.plot(dataset.loc[dataset.trend == -1.0].index,
+             dataset.adj_close[dataset.trend == -1.0],
+             '^', markersize=8, color='r', label='down')
+
+    plotly_fig = mpl_to_plotly(plt1)
+    graph = dcc.Graph(id='myGraph', fig=plotly_fig)
+    return graph
+
+
+if __name__ == '__main__':
+    dataframe = pandas.read_csv("data/BTC_crypto_data.csv")
+
+    graph = plot_graph(dataframe)
+
+    app.layout = html.Div([
+        dcc.Graph(id='matplotlib-graph', figure=graph)
+
+    ])
+
+    app.run_server(debug=True, port=8010, host='localhost')
