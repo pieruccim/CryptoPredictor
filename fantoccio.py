246da841a63663a699ea8791aca8eb534ae703c1
Matteo Pierucci <matteo.pierucci4@gmail.com>
2022-03-16 09:33:27 +0100

    fantoccio.py to test Updater class

--- fantoccio.py
+++ fantoccio.py
@@ -1,7 +1,14 @@
 from persistence.MongoConnector import MongoConnector
+from updater import Updater
 
+#result = MongoConnector.get_collection('bitcoin').find()
+#print(result.next())
 
-result = MongoConnector.get_collection('bitcoin').find()
-print(result.next())
+up = Updater()
+#dataframe = up.get_missing_tuples('BTC')
 
+#final_df = up.compute_statistical_values(original_df=dataframe)
 
+#up.store_missing_tuples('BTC', final_df)
+
+up.update_currencies_collections()
